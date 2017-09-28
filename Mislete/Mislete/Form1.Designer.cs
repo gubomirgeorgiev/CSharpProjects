0aeb8d8dffdcec4f07dc384aa27343fad496ea92
Lyubomir Georgiev <gubomirgeorgiev@gmail.com>
2017-09-28 17:57:55 +0300

    Framework created: 1. buttons actions added for: Forward; Backward; Refresh; Go 2.Opening web page by pressing enter key implemented

---
+++ Mislete/Mislete/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Mislete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.backwardBtn = new System.Windows.Forms.ToolStripButton();
+            this.forwardBtn = new System.Windows.Forms.ToolStripButton();
+            this.refreshBtn = new System.Windows.Forms.ToolStripButton();
+            this.homeBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.label1 = new System.Windows.Forms.Label();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backwardBtn,
+            this.forwardBtn,
+            this.refreshBtn,
+            this.homeBtn,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(893, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // backwardBtn
+            // 
+            this.backwardBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.backwardBtn.Image = ((System.Drawing.Image)(resources.GetObject("backwardBtn.Image")));
+            this.backwardBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backwardBtn.Name = "backwardBtn";
+            this.backwardBtn.Size = new System.Drawing.Size(23, 22);
+            this.backwardBtn.Text = "backwardBtn";
+            this.backwardBtn.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // forwardBtn
+            // 
+            this.forwardBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.forwardBtn.Image = ((System.Drawing.Image)(resources.GetObject("forwardBtn.Image")));
+            this.forwardBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardBtn.Name = "forwardBtn";
+            this.forwardBtn.Size = new System.Drawing.Size(23, 22);
+            this.forwardBtn.Text = "forwardBtn";
+            this.forwardBtn.Click += new System.EventHandler(this.forwardBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refreshBtn.Image = ((System.Drawing.Image)(resources.GetObject("refreshBtn.Image")));
+            this.refreshBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(23, 22);
+            this.refreshBtn.Text = "refreshBtn";
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.homeBtn.Image = ((System.Drawing.Image)(resources.GetObject("homeBtn.Image")));
+            this.homeBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Size = new System.Drawing.Size(23, 22);
+            this.homeBtn.Text = "homeBtn";
+            this.homeBtn.Click += new System.EventHandler(this.toolStripButton1_Click_1);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Address:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(832, 2);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(31, 23);
+            this.goBtn.TabIndex = 4;
+            this.goBtn.Text = "GO";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(893, 479);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(162, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(664, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragDrop);
+            this.comboBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.comboBox1_DragEnter);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            this.comboBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 504);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mislete";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton backwardBtn;
+        private System.Windows.Forms.ToolStripButton forwardBtn;
+        private System.Windows.Forms.ToolStripButton refreshBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton homeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Mislete/Mislete/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mislete
+{
+    public partial class Form1 : Form
+    {
+        string homePage = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // Backward button setup
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (webApp.CanGoBack)
+            {
+                webApp.GoBack();
+            }
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        // Forward button setup
+        private void forwardBtn_Click(object sender, EventArgs e)
+        {
+            if (webApp.CanGoForward)
+            {
+                webApp.GoForward();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        WebBrowser webApp = new WebBrowser();
+        int i = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webApp = new WebBrowser();
+            webApp.ScriptErrorsSuppressed = true;
+            webApp.Dock = DockStyle.Fill;
+            webApp.Visible = true;
+            //webApp.DocumentCompleted += ;
+            tabControl1.TabPages.Add("New Tab");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(webApp);
+            i += i;
+        }
+        /*
+        // Start a drag that copies text.
+        private void lblDragSource_MouseDown(object sender,
+            MouseEventArgs e)
+        {
+            // Start the drag if it's the right mouse button.
+            if (e.Button == MouseButtons.Right)
+            {
+                //Drag Source
+                comboBox1.DoDragDrop("Here's some text!",
+                    DragDropEffects.Copy);
+            }
+        }
+        // Indicate that we can accept a copy of text.
+        private void dragAndDropHomepageFromTextBox (object sender, DragEventArgs e)
+        {
+            // See if this is a copy and the data includes text.
+            if ((e.Data.GetDataPresent(DataFormats.Text))&&(e.AllowedEffect & DragDropEffects.Copy) != 0)
+            {
+                // Allow this.
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                // Don't allow any other drop.
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        // Accept the drop.
+        private void lblDropTarget_DragDrop(object sender, DragEventArgs e)
+        {
+            //Drop Target
+            homeBtn.Text = (string)e.Data.GetData(DataFormats.Text);
+        }*/
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            
+            if (homePage.Equals(""))
+            {
+                homePage = comboBox1.Text;
+                webApp.Navigate(homePage);
+            }
+            else
+            {
+                webApp.Navigate(homePage);
+            }
+        }
+        //SETTING UP REFRESH BUTTON
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            if (webApp.Url.Equals(comboBox1.Text))
+            {
+                webApp.Refresh();
+            }
+        }
+        //defininf mouse drop action to make copy or move action
+        private void comboBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ComboBox tb = (ComboBox)sender;
+            tb.SelectAll();
+            tb.DoDragDrop(tb.Text, DragDropEffects.Copy | DragDropEffects.Move);
+        }
+        // See if this is a copy and the data includes text.
+        private void comboBox1_DragEnter (object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                // Allow this.
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                // Don't allow any other drop.
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comboBox1.DoDragDrop(comboBox1.Text, DragDropEffects.Copy | DragDropEffects.Move);
+
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            webApp.Navigate(comboBox1.Text);
+        }
+
+        private void comboBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ComboBox tb = (ComboBox)sender;
+            tb.Text = (string)e.Data.GetData(DataFormats.Text);
+            homePage = tb.Text;
+            //homePage = comboBox1.Text;
+        }
+        private void homeBtn_Drag_Drop_Text_Load (object sender, EventArgs e)
+        {
+            homeBtn.DragEnter += new DragEventHandler(comboBox1_DragEnter);
+            homeBtn.MouseDown += new MouseEventHandler(comboBox1_MouseDown);
+            homeBtn.DragDrop += new DragEventHandler(comboBox1_DragDrop);
+
+            comboBox1.DragEnter += new DragEventHandler(comboBox1_DragEnter);
+            comboBox1.MouseDown += new MouseEventHandler(comboBox1_MouseDown);
+            comboBox1.DragDrop += new DragEventHandler(comboBox1_DragDrop);
+        }
+
+        //Opening web page by pressing enter key
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                webApp.Navigate(comboBox1.Text);
+            }
+        }
+    }
+}
