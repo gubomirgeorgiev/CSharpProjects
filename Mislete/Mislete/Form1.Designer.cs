5eb3ca155b050f4be4472a52821a814d4d1c4da3
Lyubomir Georgiev <gubomirgeorgiev@gmail.com>
2017-09-30 01:18:19 +0300

    icons changed

--- Mislete/Mislete/Form1.Designer.cs
+++ Mislete/Mislete/Form1.Designer.cs
@@ -49,6 +49,7 @@ namespace Mislete
             // 
             // toolStrip1
             // 
+            this.toolStrip1.AutoSize = false;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.backwardBtn,
             this.forwardBtn,
@@ -62,7 +63,7 @@ namespace Mislete
             this.removePageBtn});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1095, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1095, 40);
             this.toolStrip1.Stretch = true;
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
@@ -72,9 +73,10 @@ namespace Mislete
             // 
             this.backwardBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.backwardBtn.Image = ((System.Drawing.Image)(resources.GetObject("backwardBtn.Image")));
+            this.backwardBtn.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.backwardBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.backwardBtn.Name = "backwardBtn";
-            this.backwardBtn.Size = new System.Drawing.Size(23, 22);
+            this.backwardBtn.Size = new System.Drawing.Size(31, 37);
             this.backwardBtn.Text = "backwardBtn";
             this.backwardBtn.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
@@ -82,9 +84,10 @@ namespace Mislete
             // 
             this.forwardBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.forwardBtn.Image = ((System.Drawing.Image)(resources.GetObject("forwardBtn.Image")));
+            this.forwardBtn.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.forwardBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.forwardBtn.Name = "forwardBtn";
-            this.forwardBtn.Size = new System.Drawing.Size(23, 22);
+            this.forwardBtn.Size = new System.Drawing.Size(31, 37);
             this.forwardBtn.Text = "forwardBtn";
             this.forwardBtn.Click += new System.EventHandler(this.forwardBtn_Click);
             // 
@@ -92,9 +95,10 @@ namespace Mislete
             // 
             this.refreshBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.refreshBtn.Image = ((System.Drawing.Image)(resources.GetObject("refreshBtn.Image")));
+            this.refreshBtn.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.refreshBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.refreshBtn.Name = "refreshBtn";
-            this.refreshBtn.Size = new System.Drawing.Size(23, 22);
+            this.refreshBtn.Size = new System.Drawing.Size(31, 37);
             this.refreshBtn.Text = "refreshBtn";
             this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
             // 
@@ -102,67 +106,74 @@ namespace Mislete
             // 
             this.homeBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.homeBtn.Image = ((System.Drawing.Image)(resources.GetObject("homeBtn.Image")));
+            this.homeBtn.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.homeBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.homeBtn.Name = "homeBtn";
-            this.homeBtn.Size = new System.Drawing.Size(23, 22);
+            this.homeBtn.Size = new System.Drawing.Size(31, 37);
             this.homeBtn.Text = "homeBtn";
             this.homeBtn.Click += new System.EventHandler(this.toolStripButton1_Click_1);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 40);
             // 
             // toolStripLabel1
             // 
             this.toolStripLabel1.Name = "toolStripLabel1";
-            this.toolStripLabel1.Size = new System.Drawing.Size(56, 22);
+            this.toolStripLabel1.Size = new System.Drawing.Size(56, 37);
             this.toolStripLabel1.Text = "Address";
             // 
             // comboBox1
             // 
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(400, 25);
+            this.comboBox1.Size = new System.Drawing.Size(400, 40);
             this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
             this.comboBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseDown);
             // 
             // goBtn
             // 
             this.goBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.goBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.goBtn.Image = ((System.Drawing.Image)(resources.GetObject("goBtn.Image")));
             this.goBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.goBtn.Name = "goBtn";
-            this.goBtn.Size = new System.Drawing.Size(29, 22);
+            this.goBtn.Size = new System.Drawing.Size(31, 37);
             this.goBtn.Text = "Go";
             this.goBtn.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // AddPageBtn
             // 
             this.AddPageBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.AddPageBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.AddPageBtn.Image = ((System.Drawing.Image)(resources.GetObject("AddPageBtn.Image")));
             this.AddPageBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.AddPageBtn.Name = "AddPageBtn";
-            this.AddPageBtn.Size = new System.Drawing.Size(69, 22);
+            this.AddPageBtn.Size = new System.Drawing.Size(72, 37);
             this.AddPageBtn.Text = "Add Page";
             this.AddPageBtn.Click += new System.EventHandler(this.AddPageBtn_Click_1);
             // 
             // removePageBtn
             // 
             this.removePageBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.removePageBtn.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.removePageBtn.Image = ((System.Drawing.Image)(resources.GetObject("removePageBtn.Image")));
             this.removePageBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.removePageBtn.Name = "removePageBtn";
-            this.removePageBtn.Size = new System.Drawing.Size(92, 22);
+            this.removePageBtn.Size = new System.Drawing.Size(96, 37);
             this.removePageBtn.Text = "Remove Page";
             this.removePageBtn.Click += new System.EventHandler(this.removePageBtn_Click);
             // 
             // tabControl1
             // 
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.AllowDrop = true;
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 43);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1095, 479);
+            this.tabControl1.Size = new System.Drawing.Size(1096, 461);
             this.tabControl1.TabIndex = 5;
             // 
             // Form1
@@ -183,7 +194,6 @@ namespace Mislete
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -198,7 +208,6 @@ namespace Mislete
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton backwardBtn;
         private System.Windows.Forms.ToolStripButton forwardBtn;
         private System.Windows.Forms.ToolStripButton refreshBtn;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
@@ -209,6 +218,7 @@ namespace Mislete
         private ToolStripComboBox comboBox1;
         private ToolStripButton AddPageBtn;
         private ToolStripButton removePageBtn;
+        internal ToolStripButton backwardBtn;
     }
 }
 
